6a7bf77401fcc1e19f4b0b602b7d00f4b1d94f3d
Camille Bilodeau <camille79bilodeau@gmail.com>
2019-06-11 16:10:03 -0400

    Cleaned up clean.sh

---
+++ SamplePrep/clean.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+source ~/.bashrc
+
+# clean pdb file from raw_pdb folder and place into clean_pdb folder
+# 
+# Remove waters
+# Fix hydrogens
+# Remove heteroatoms
+
+curr=`pwd`
+raw=$curr/1raw_pdb
+clean=$curr/2clean_pdb
+temp=$curr/temp  # Files will be deleted
+
+rm -rf $temp
+mkdir $temp
+
+
+## Remove water:
+cd $raw
+for i in *.pdb
+do
+    pdb="$i"
+    fname=${pdb%.*}
+        pdb4amber -i $raw/$i -o $temp/$fname"-clean.pdb" --dry #--reduce
+done
+
+cd $curr
+mv $temp/*-clean.pdb $clean
+cd $clean
+
+## Remove heteroatoms:
+for i in *
+do
+        sed -i '/HETATM/d' $i
+done
