21b9f141636124d610f6e558dda52343fe44780a
Camille Bilodeau <camille79bilodeau@gmail.com>
2019-06-11 16:51:30 -0400

    still trying to fix things

--- SamplePrep/start.sh
+++ SamplePrep/start.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
-# Script to run everything!
+# Script to run everything
 # Source environment/bashrc
 source ~/.bashrc
+
+# Replace the following with your own environment location
 source activate /home/cbilodeau/anaconda2/envs/deepchem2
 
 module load gromacs/453
@@ -14,7 +16,7 @@ sh SASA_Calc.sh
 python SASA_Calc.py
 
 
-# 2 Create Occupancy Maps:
+## 2 Create Occupancy Maps:
 python VoxelParse.py
 
 # 3 Divide Into Patches:
